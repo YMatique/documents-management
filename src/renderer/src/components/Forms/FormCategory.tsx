e9dc44a7888107080a89eb6c7ccc3e8a23bbbe8d
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-01-06 12:10:10 +0200

    ----

--- src/renderer/src/components/Forms/FormCategory.tsx
+++ src/renderer/src/components/Forms/FormCategory.tsx
@@ -10,7 +10,7 @@ function FormCategory(): JSX.Element {
             type="email"
             id="input-label"
             name=""
-            className="py-3 px-4 block w-full ring-primary border focus:border-primary"
+            className="py-3 px-4 block w-full ring-primary border focus:border-primary outline-none"
             placeholder="you@site.com"
           />
         </div>
@@ -25,6 +25,20 @@ function FormCategory(): JSX.Element {
             placeholder="John"
             required
           />
+        </div>
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500">
+            Your name
+          </label>
+          <input
+            type="text"
+            id="success"
+            className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg outline-none focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+            placeholder="Success input"
+          />
+          <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+            <span className="font-medium">Well done!</span> Some success message.
+          </p>
         </div>
       </div>
     </form>
