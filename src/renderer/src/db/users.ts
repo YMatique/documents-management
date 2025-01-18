981bc127c6fee1dc9651e08202d558c0cbb48602
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-01-18 03:05:52 +0200

    -----

---
+++ src/renderer/src/db/users.ts
@@ -0,0 +1,15 @@
+// import { User } from "@prisma/client";
+// import prisma from "./db";
+
+// async function createUser(): Promise<User> {
+//   const user = await prisma.user.create({
+//     data: {
+//       name: "Advogado Exemplo",
+//       email: "advogado@email.com",
+//       password: "senha123",
+//     },
+//   });
+// //   console.log(user);
+// }
+
+// createUser();
